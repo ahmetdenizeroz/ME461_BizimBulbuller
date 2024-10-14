4d11acbafc3483a75259be82fb0fe1c85449485f
Deniz <ahmetdenniseroz@gmail.com>
2024-10-14 23:41:08 +0300

    Updating Lastest Version ADE

--- Second_HW/ball.py
+++ Second_HW/ball.py
@@ -15,11 +15,11 @@ class ball():
     def Change_Dir(ball, unit_dir_of_obst):
     
         projection_onto = unit_dir_of_obst[0] * ball.velocity[0] + unit_dir_of_obst[1] * ball.velocity[1]
-        # porisiton of - in taking normal of the obstacle is adjusted for opencv frame since it is starts from top left 
-        projection_normal = unit_dir_of_obst[1] * ball.velocity[0] + -unit_dir_of_obst[0] * ball.velocity[1]
+        # positon of - in taking normal of the obstacle is adjusted for opencv frame since it is starts from top left 
+        projection_normal = -unit_dir_of_obst[1] * ball.velocity[0] + unit_dir_of_obst[0] * ball.velocity[1]
 
         obst_angle = math.atan2(unit_dir_of_obst[1], unit_dir_of_obst[0])
-        dep_angle = obst_angle + math.atan2(projection_normal, projection_onto)
+        dep_angle = obst_angle + math.atan2(-projection_normal, projection_onto)
 
         ball.velocity = (math.cos(dep_angle) * ball.speed, math.sin(dep_angle) * ball.speed)
 
