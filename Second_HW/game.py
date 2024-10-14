ce9952d788e6c78e04f673a7213cb8e01d5722e0
Deniz <ahmetdenniseroz@gmail.com>
2024-10-14 23:44:24 +0300

    Updating Lastest Version -ADE

--- Second_HW/game.py
+++ Second_HW/game.py
@@ -1,26 +1,46 @@
 import ball
 import cv2
 import mediapipe as mp
+import random
 
 # Variables
-w = 1280
-h = 720
+w = 1920
+h = 1080
+target_pos = (random.randint(w/20, 19*w/20),random.randint(w/20, h - w/20))
+#target_pos = (int(w/2) ,int(h/2))
+is_target_hit = False
+point = 0
+font = cv2.FONT_HERSHEY_SIMPLEX
+is_hit = False
+
+is_hit_left = False
+is_hit_right = False
+is_hit_bottom = False
+is_hit_top = False
+
+# Functions
+def distance(point1, point2):
+    length = ((point2[0]-point1[0])**2 + (point2[1]-point1[1])**2)**0.5
+    vector = (point2[0] - point1[0], point2[1] - point1[1])
+    return length, vector
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 # Video Capture
 capture = cv2.VideoCapture(0)
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, w)
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-ball = ball.ball((90,90,90), 20, (int(w/2), int(h/2)), (20, 20))
+ball = ball.ball((90,90,90), int(w/40), (int(w/2), int(h/2)), (50, 50))
 
-with mp_hands.Hands(min_detection_confidence = 0.4, min_tracking_confidence = 0.4) as hands:
+
+with mp_hands.Hands(min_detection_confidence = 0.2, min_tracking_confidence = 0.2) as hands:
     while capture.isOpened():
         ret, frame = capture.read()
+        frame = cv2.resize(frame, (1920, 1080))
         if not ret:
-            print("Chack your Webcam")
+            print("Check your Webcam")
             break
 
         # frame flip
@@ -29,26 +49,83 @@ with mp_hands.Hands(min_detection_confidence = 0.4, min_tracking_confidence = 0.
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         results = hands.process(rgb_frame)
-
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS, mp_drawing.DrawingSpec(color = (111, 111, 111), thickness = 2), mp_drawing.DrawingSpec(color = (5, 5, 5), thickness = 2))
-       
+	#Ball Drawing
         ball.move()
-
-        if ball.position[0] < 10: 
-            ball.Change_Dir((0, 1))
-        if ball.position[0] > 1270:
-            ball.Change_Dir((0, -1))
-
-        if ball.position[1] < 10:
-            ball.Change_Dir((-1, 0))
-        if ball.position[1] > 710:
-            ball.Change_Dir((1,0))
-        print(ball.velocity)
-        print(ball.position)
         cv2.circle(frame, ball.position, ball.size, ball.color, -1)
 
+        #target
+
+        cv2.circle(frame, target_pos,int(w/20),(255,46,175),-1)
+
+
+        if results.multi_hand_landmarks:
+            #print(type(results.multi_hand_landmarks))
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS, mp_drawing.DrawingSpec(color = (111, 111, 111), thickness = 2), mp_drawing.DrawingSpec(color = (5, 5, 5), thickness = 2))
+                
+            wrist1 = results.multi_hand_landmarks[0].landmark[0]
+            knuckle11 = results.multi_hand_landmarks[0].landmark[5]
+            knuckle41 = results.multi_hand_landmarks[0].landmark[17] 
+            point1 = (int((knuckle11.x * w * 2+ wrist1.x * w+ knuckle41.x * w*2)/5), int((knuckle11.y * h * 2+ wrist1.y * h+ knuckle41.y * h*2)/5))
+
+            if len(results.multi_hand_landmarks) > 1:
+                wrist2 = results.multi_hand_landmarks[1].landmark[0]
+                knuckle12 = results.multi_hand_landmarks[1].landmark[5]
+                knuckle42 = results.multi_hand_landmarks[1].landmark[17]
+                point2 = (int((knuckle12.x * w * 2+ wrist2.x * w+ knuckle42.x * w*2)/5), int((knuckle12.y * h * 2+ wrist2.y * h+ knuckle42.y * h*2)/5)) 
+
+                if distance(point2, point1)[0] < w/4:
+                    cv2.line(frame, point1, point2, (5,255,46), 5)
+
+                    dx = (point1[0]-point2[0]) / distance(point2, point1)[0]
+                    dy = (point1[1]-point2[1]) / distance(point2, point1)[0]
+
+                    midpoint = ((point1[0]+point2[0])/2, (point1[1]+point2[1])/2)
+
+                    if distance(midpoint, ball.position)[0] < distance(point2, point1)[0]/2:
+                        normal_dist = distance(midpoint, ball.position)[1][0]*-dy + distance(midpoint, ball.position)[1][1] * dx
+                        if abs(normal_dist) -ball.size < 1 and not is_hit:
+                            ball.Change_Dir((dx, dy))
+                            is_hit = True
+                        if abs(normal_dist) -ball.size > 1:
+                            is_hit = False
+
+        if ball.position[0] - ball.size < 1 and not is_hit_left: 
+            ball.Change_Dir((0, 1))
+            is_hit_left = True
+        else:
+            is_hit_left = False
+
+        if ball.position[0] + ball.size > w - 1 and not is_hit_right:
+            ball.Change_Dir((0, 1))
+            is_hit_right = True
+        else:
+            is_hit_right = False
+
+        if ball.position[1] -ball.size < 1 and not is_hit_top:
+            ball.Change_Dir((1, 0))
+            is_hit_top = True
+        else:
+            is_hit_top = False
+
+        if ball.position[1] + ball.size > h - 1  and not is_hit_bottom:
+            ball.Change_Dir((1,0))
+            is_hit_bottom = True
+        else:
+            is_hit_bottom = False
+
+        if distance(target_pos, ball.position)[0] < w/20 and not is_target_hit:
+            point += 10
+            is_target_hit = True
+
+        if distance(target_pos, ball.position)[0] > w/20:
+            is_target_hit = False
+        
+        cv2.putText(frame, f"Score: {point}", (10, 30), font, 1, (255, 255, 255), 2)
+
+        #print(point)
+        #print(ball.velocity)
+        #print(ball.position)
         cv2.imshow("Game", frame)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
